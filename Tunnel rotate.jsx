cd1d4d46141d0667ed9a7fd649829ea5c60abb7d
Charlton Groves <charltongroves@gmail.com>
2016-08-02 20:29:18 +1000

    Fixed last build.

--- Tunnel rotate.jsx
+++ Tunnel rotate.jsx
@@ -1,21 +1,33 @@
 ﻿var originalPhoto = app.activeDocument.activeLayer;
-var originalPhotoDup = originalPhoto.duplicate();
-var ANGLE = 2; // Determines how much the layer is rotated
-var xstart = 1; //DONT CHANGE
-var xend = ((app.activeDocument.width * (1.0/2.54) * app.activeDocument.resoltion) | 0) - 1; //SET TO WIDTH OF DOCUMENT - 1
-var ystart = 1; //DONT CHANGE
-var yend = ((app.activeDocument.height * (1.0/2.54) * app.activeDocument.resoltion) | 0) - 1; //SET TO HEIGHT OF DOCUMENT - 1
-alert(xend.toString());
-alert(yend.toString());
-var INC = 50; // Determines how big the size difference is between each layer
-var repetitions = 16; //how many layers are made
+/*
+    hey there.
+    This script works best on images with a 1:1 ratio (square images). so crop em to that before you run it
+    have fun!
+*/
 
+// VARIABLES YOU CAN CHANGE!
+var repetitions = 15; //How many layers are made per side
+var ANGLE = 2; // Determines how much the layer is rotated
+
+//DON'T TOUCH STUFF BELOW HERE
+var xstart = 1; //DON'T CHANGE
+var ratio = 1.0/2.54; // ratio between cm and inches, photoshop is crap and uses both metric and imperial >:(  DON'T CHANGE
+var resolution = app.activeDocument.resolution;
+var xend = ((parseFloat(app.activeDocument.width.toString())  * ratio* resolution) | 0) - 1; //SET TO WIDTH OF DOCUMENT - 1
+var ystart = 1; //DON'T CHANGE
+var yend = ((parseFloat(app.activeDocument.height.toString()) * ratio* resolution) | 0) - 1; //SET TO HEIGHT OF DOCUMENT - 1
+var INC = 50; // DON'T CHANGE
+if (xend > yend) {
+    INC = yend / (2*repetitions);
+} else { 
+    INC = xend / (2*repetitions);
+}
 function TunnelOneSide(side, ANGLE, xstart, xend, ystart, yend, INC, repetitions) {
     if (side !== 1) {
         ANGLE = ANGLE * -1
     }
     for (var i = 1; i < repetitions; i++) {
-            var newPhoto = originalPhotoDup.duplicate();
+            var newPhoto = originalPhoto.duplicate();
             app.activeDocument.activeLayer = newPhoto;
             //activeDocument.activeLayer.move( activeDocument, ElementPlacement.PLACEATBEGINNING ); 
             var selRegion = Array(Array(xstart,ystart),
